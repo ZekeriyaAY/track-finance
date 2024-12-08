a0361f3a3c422e043c705e3f19cd362d1cc4366f
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-12-08 21:45:13 +0300

    finance modules added

--- app/__init__.py
+++ app/__init__.py
@@ -33,17 +33,26 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.category import bp as category_bp
+    app.register_blueprint(category_bp)
+
+    from app.brand import bp as brand_bp
+    app.register_blueprint(brand_bp)
+
+    from app.transaction import bp as transaction_bp
+    app.register_blueprint(transaction_bp)
+
     if not app.debug and not app.testing:
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
                                         backupCount=10)
         file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
+        app.logger.info('Track Finance App startup')
 
     return app
 
--- app/models.py
+++ app/models.py
@@ -21,29 +21,11 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
-    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
 
-    followers = sa.Table(
-        'followers',
-        db.metadata,
-        sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
-                  primary_key=True),
-        sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
-                  primary_key=True)
-    )
-    following: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        back_populates='followers')
-    followers: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.followed_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
-        back_populates='following')
+    transactions: so.WriteOnlyMapped['Transaction'] = so.relationship(
+        back_populates='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -58,53 +40,53 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
-    def follow(self, user):
-        if not self.is_following(user):
-            self.following.add(user)
 
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.following.remove(user)
-
-    def is_following(self, user):
-        query = self.following.select().where(User.id == user.id)
-        return db.session.scalar(query) is not None
-
-    def followers_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.followers.select().subquery())
-        return db.session.scalar(query)
-
-    def following_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.following.select().subquery())
-        return db.session.scalar(query)
-
-    def following_posts(self):
-        Author = so.aliased(User)
-        Follower = so.aliased(User)
-        return (
-            sa.select(Post)
-            .join(Post.author.of_type(Author))
-            .join(Author.followers.of_type(Follower), isouter=True)
-            .where(sa.or_(
-                Follower.id == self.id,
-                Author.id == self.id,
-            ))
-            .group_by(Post)
-            .order_by(Post.timestamp.desc())
-        )
-
-
-class Post(db.Model):
+class Category(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True, nullable=False)
+    name: so.Mapped[str] = so.mapped_column(
+        sa.String(64), index=True, unique=True, nullable=False)
 
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    transactions: so.WriteOnlyMapped['Transaction'] = so.relationship(
+        back_populates='category', lazy='dynamic')
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'<Category {self.name}>'
+
+
+class Brand(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True, nullable=False)
+    name: so.Mapped[str] = so.mapped_column(
+        sa.String(64), index=True, unique=True, nullable=False)
+
+    transactions: so.WriteOnlyMapped['Transaction'] = so.relationship(
+        back_populates='brand', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Brand {self.name}>'
+
+
+class Transaction(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True, nullable=False)
+    category_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(Category.id), index=True, nullable=False)
+    brand_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(Brand.id), index=True, nullable=False)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), nullable=False)
+    amount: so.Mapped[Optional[float]] = so.mapped_column(
+        sa.Float, nullable=False)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+
+    user: so.Mapped[User] = so.relationship(back_populates='transactions')
+    category: so.Mapped[Category] = so.relationship(
+        back_populates='transactions')
+    brand: so.Mapped[Brand] = so.relationship(back_populates='transactions')
+
+    def __repr__(self):
+        return f'<Transaction {self.name}>'
