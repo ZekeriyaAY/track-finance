cf0109d3897e73e283730f6a23e3599ef8959b6e
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-12-08 21:45:28 +0300

    posts deleted

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,14 +1,13 @@
 from app import db
 from app.models import User
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
 import sqlalchemy as sa
 
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
@@ -21,13 +20,3 @@ class EditProfileForm(FlaskForm):
                 User.username == username.data))
             if user is not None:
                 raise ValidationError('Please use a different username.')
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
-
-
-class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')
