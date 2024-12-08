091ce7070861a37d58a902c8bed0a2d7ec08b16f
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-12-08 21:43:42 +0300

    brand added

---
+++ app/brand/forms.py
@@ -0,0 +1,17 @@
+from app import db
+from app.models import Brand
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+import sqlalchemy as sa
+
+
+class BrandForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_name(self, name):
+        brand = db.session.scalar(sa.select(Brand).where(
+            Brand.name == name.data))
+        if brand is not None:
+            raise ValidationError('Please use a different brand name.')
---
+++ app/brand/routes.py
@@ -0,0 +1,53 @@
+from app import db
+from app.brand import bp
+from app.brand.forms import BrandForm
+from app.models import Brand
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
+import sqlalchemy as sa
+
+
+@bp.route('/brand/add', methods=['GET', 'POST'], endpoint='add_brand')
+@login_required
+def add_brand():
+    form = BrandForm()
+    if form.validate_on_submit():
+        brand = Brand(user_id=current_user.id, name=form.name.data)
+        db.session.add(brand)
+        db.session.commit()
+        flash('Brand added. {}#{}'.format(brand.name, brand.id))
+        return redirect(url_for('brand.add_brand'))
+    return render_template('addit_brand.html', form=form)
+
+
+@bp.route('/brand/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_brand(id):
+    brand = db.first_or_404(sa.select(Brand).where(
+        Brand.id == id, Brand.user_id == current_user.id))
+    db.session.delete(brand)
+    db.session.commit()
+    flash('Brand deleted. {}#{}'.format(brand.name, brand.id))
+    return redirect(url_for('brand.list_brand'))
+
+
+@bp.route('/brand')
+@login_required
+def list_brand():
+    brands = db.session.scalars(
+        sa.select(Brand).where(Brand.user_id == current_user.id))
+    return render_template('list_brand.html', brands=brands)
+
+
+@bp.route('/brand/<int:id>/edit', methods=['GET', 'POST'], endpoint='edit_brand')
+@login_required
+def edit_brand(id):
+    brand = db.first_or_404(sa.select(Brand).where(
+        Brand.id == id, Brand.user_id == current_user.id))
+    form = BrandForm(obj=brand)
+    if form.validate_on_submit():
+        brand.name = form.name.data
+        db.session.commit()
+        flash('Brand updated. {}#{}'.format(brand.name, brand.id))
+        return redirect(url_for('brand.list_brand'))
+    return render_template('addit_brand.html', form=form)
