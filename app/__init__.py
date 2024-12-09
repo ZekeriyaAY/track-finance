524c35158f5424cc2576bf82fd34ad774a649211
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-12-10 00:20:00 +0300

    profile module added

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,9 @@ def create_app(config_class=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
+    from app.user import bp as user_bp
+    app.register_blueprint(user_bp)
+
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,6 @@
 from app import db
 from app.main import bp
 from app.models import User
-from app.main.forms import EditProfileForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
 import sqlalchemy as sa
@@ -19,25 +18,3 @@ def before_request():
 @login_required
 def index():
     return render_template('index.html', title='Home')
-
-
-@bp.route('/user/<username>')
-@login_required
-def user(username):
-    user = db.first_or_404(sa.select(User).where(User.username == username))
-    return render_template('user.html', title='Profile', user=user)
-
-
-@bp.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm(current_user.username)
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('main.edit_profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
