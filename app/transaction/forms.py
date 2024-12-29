49b824592a44e3e2fb7991779a1a75b190774167
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-12-29 23:53:24 +0300

    refactor: Update transaction form
    - Rename form fields to match model changes
    - Update category_id and brand_id fields
    - Add proper validation for deleted items

--- app/transaction/forms.py
+++ app/transaction/forms.py
@@ -1,4 +1,3 @@
-
 from app import db
 from app.models import Transaction
 from flask_wtf import FlaskForm
@@ -10,6 +9,6 @@ import sqlalchemy as sa
 class TransactionForm(FlaskForm):
     name = StringField('Transaction Name', validators=[DataRequired()])
     amount = FloatField('Amount', validators=[DataRequired()])
-    category = SelectField('Category', coerce=int, validators=[DataRequired()])
-    brand = SelectField('Brand', coerce=int, validators=[DataRequired()])
+    category_id = SelectField('Category', coerce=int, validators=[DataRequired()])
+    brand_id = SelectField('Brand', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Submit')
