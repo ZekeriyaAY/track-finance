43b290bf9faf9139ba2fb2163c62d6e67396aa18
Zekeriya AY <zekeriya@zekeriyaay.com>
2025-01-01 22:41:10 +0300

    refactor(i18n): translate transaction route flash messages to English
    - Update add transaction success message
    - Update delete transaction success message
    - Update edit transaction success message
    - Improve message formatting with quotes around transaction names

--- app/transaction/routes.py
+++ app/transaction/routes.py
@@ -8,43 +8,6 @@ import sqlalchemy as sa
 from datetime import datetime, timezone
 
 
-@bp.route('/transaction/add', methods=['GET', 'POST'], endpoint='add_transaction')
-@login_required
-def add_transaction():
-    form = TransactionForm()
-    form.category_id.choices = [(c.id, f'{c.name} [{c.type}]') for c in Category.query.filter_by(
-        user_id=current_user.id, is_deleted=False).all()]
-    form.brand_id.choices = [(b.id, b.name) for b in Brand.query.filter_by(
-        user_id=current_user.id, is_deleted=False).all()]
-    
-    if form.validate_on_submit():
-        transaction = Transaction(
-            user_id=current_user.id,
-            category_id=form.category_id.data,
-            brand_id=form.brand_id.data,
-            name=form.name.data,
-            amount=form.amount.data,
-            timestamp=datetime.now(timezone.utc)
-        )
-        db.session.add(transaction)
-        db.session.commit()
-        flash(f'Transaction added: {transaction.name}#{transaction.id}', 'success')
-        return redirect(url_for('transaction.list_transaction'))
-    return render_template('addit_transaction.html', title='Add New Transaction', form=form)
-
-
-@bp.route('/transaction/<int:id>/delete', methods=['POST'])
-@login_required
-def delete_transaction(id):
-    transaction = db.first_or_404(sa.select(Transaction).where(
-        Transaction.id == id, Transaction.user_id == current_user.id))
-    db.session.delete(transaction)
-    db.session.commit()
-    flash('Transaction deleted. {}#{}'.format(
-        transaction.name, transaction.id), 'success')
-    return redirect(url_for('transaction.list_transaction'))
-
-
 @bp.route('/transaction')
 @login_required
 def list_transaction():
@@ -58,6 +21,42 @@ def list_transaction():
     return render_template('list_transaction.html', title='Transactions', transactions=transactions)
 
 
+@bp.route('/transaction/add', methods=['GET', 'POST'], endpoint='add_transaction')
+@login_required
+def add_transaction():
+    form = TransactionForm()
+    form.category_id.choices = [(c.id, f'{c.name} [{c.type}]') for c in Category.query.filter_by(
+        user_id=current_user.id, is_deleted=False).all()]
+    form.brand_id.choices = [(b.id, b.name) for b in Brand.query.filter_by(
+        user_id=current_user.id, is_deleted=False).all()]
+
+    if form.validate_on_submit():
+        transaction = Transaction(
+            user_id=current_user.id,
+            category_id=form.category_id.data,
+            brand_id=form.brand_id.data,
+            name=form.name.data,
+            amount=form.amount.data,
+            timestamp=datetime.now(timezone.utc)
+        )
+        db.session.add(transaction)
+        db.session.commit()
+        flash(f'Transaction "{transaction.name}" has been added successfully.', 'success')
+        return redirect(url_for('transaction.list_transaction'))
+    return render_template('addit_transaction.html', title='Add New Transaction', form=form)
+
+
+@bp.route('/transaction/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_transaction(id):
+    transaction = db.first_or_404(sa.select(Transaction).where(
+        Transaction.id == id, Transaction.user_id == current_user.id))
+    db.session.delete(transaction)
+    db.session.commit()
+    flash(f'Transaction "{transaction.name}" has been deleted successfully.', 'info')
+    return redirect(url_for('transaction.list_transaction'))
+
+
 @bp.route('/transaction/<int:id>/edit', methods=['GET', 'POST'], endpoint='edit_transaction')
 @login_required
 def edit_transaction(id):
@@ -66,29 +65,32 @@ def edit_transaction(id):
         Transaction.user_id == current_user.id
     ))
     form = TransactionForm(obj=transaction)
-    
+
     # Aktif kategorileri al
     active_categories = Category.query.filter_by(
         user_id=current_user.id, is_deleted=False).all()
     current_category = transaction.category
-    
+
     # Mevcut kategori silinmişse onu da ekle
-    category_choices = [(c.id, f'{c.name} [{c.type}]') for c in active_categories]
+    category_choices = [(c.id, f'{c.name} [{c.type}]')
+                        for c in active_categories]
     if current_category.is_deleted and (current_category.id, f'{current_category.name} [{current_category.type}]') not in category_choices:
-        category_choices.append((current_category.id, f'{current_category.name} [{current_category.type}]'))
-    
+        category_choices.append(
+            (current_category.id, f'{current_category.name} [{current_category.type}]'))
+
     form.category_id.choices = category_choices
-    
+
     # Brand seçenekleri için benzer işlem
-    active_brands = Brand.query.filter_by(user_id=current_user.id, is_deleted=False).all()
+    active_brands = Brand.query.filter_by(
+        user_id=current_user.id, is_deleted=False).all()
     current_brand = transaction.brand
-    
+
     brand_choices = [(b.id, b.name) for b in active_brands]
     if current_brand.is_deleted and (current_brand.id, current_brand.name) not in brand_choices:
         brand_choices.append((current_brand.id, current_brand.name))
-    
+
     form.brand_id.choices = brand_choices
-    
+
     if form.validate_on_submit():
         transaction.category_id = form.category_id.data
         transaction.brand_id = form.brand_id.data
@@ -96,7 +98,6 @@ def edit_transaction(id):
         transaction.amount = form.amount.data
         transaction.timestamp = datetime.now(timezone.utc)
         db.session.commit()
-        flash('Transaction updated. {}#{}'.format(
-            transaction.name, transaction.id), 'success')
+        flash(f'Transaction "{transaction.name}" has been updated successfully.', 'success')
         return redirect(url_for('transaction.list_transaction'))
     return render_template('addit_transaction.html', title='Edit Transaction', form=form)
